6183c776111a90ed780b2217efb8d1de5fb5d508
wadzifa25 <zifawadzifa@gmail.com>
2024-03-25 10:14:02 +0700

    tugas pertemuan 1

---
+++ application/controllers/Tugas1.php
@@ -0,0 +1,12 @@
+<?php
+class Tugas1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Wadzifa
+            Saya tinggal di Kecamatan Sungai Ambawang
+            kegiatan yang saya gemari adalah
+            Menonton Drama Korea";
+    }
+}
