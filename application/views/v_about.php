9c477381eda1fbd947dc4c5e32d6fd2be203669b
wadzifa25 <zifawadzifa@gmail.com>
2024-04-22 10:02:18 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Wadzifa</li>
+    </ul>
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Poros Tengah Dusun Sadong</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pemangkat</li>
+    </ul>
+    
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Rebahan</li>
+        <li>Tidur</li>
+    </ul>
+</section>
