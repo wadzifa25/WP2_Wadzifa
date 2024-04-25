1c63abea87b52b2b4fd1ad9e5b425dfd7f64184a
wadzifa25 <zifawadzifa@gmail.com>
2024-04-25 17:07:52 +0700

    Pertemuan 5 dan Tugas 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -10,13 +10,13 @@ class Matakuliah extends CI_Controller
         $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
             'required|min_length[3]', [
             'required' => 'Kode Matakuliah Harus diisi',
-            'min_length' => 'Kode terlalu pendek'
+            'min_lenght' => 'Kode terlalu pendek'
         ]);
 
         $this->form_validation->set_rules('nama', 'Nama Matakuliah', 
             'required|min_length[3]', [
             'required' => 'Nama Matakuliah Harus diisi',
-            'min_length' => 'Nama terlalu pendek'
+            'min_lenght' => 'Nama terlalu pendek'
         ]);
 
         $this->form_validation->set_rules('sks', 'SKS', 
